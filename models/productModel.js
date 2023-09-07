68201b27a4bd386815d00aafb61bbef50423181a
najwa <najwa.kv20@gmail.com>
2023-09-07 20:57:22 +0530

    productModel created

--- models/productModel.js
+++ models/productModel.js
@@ -2,7 +2,20 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+    },
+    image: {
+        type: String,
+    }
 });
 
 const productModel = mongoose.model("Products", productSchema);
