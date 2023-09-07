3f559fd21fa7761ca437cae5b6219769a03d74a6
najwa <najwa.kv20@gmail.com>
2023-09-07 20:38:56 +0530

    adminModel created

--- models/adminModel.js
+++ models/adminModel.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const adminSchema = new Schema({
-
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    }
 });
 
 const adminModel = mongoose.model("Admin", adminSchema);
