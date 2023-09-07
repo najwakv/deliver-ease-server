2bff61e29ccea5132d7505f94d74e5a764049c49
najwa <najwa.kv20@gmail.com>
2023-09-07 21:04:13 +0530

    vendorModel created

--- models/vendorModel.js
+++ models/vendorModel.js
@@ -2,7 +2,24 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const vendorSchema = new Schema({
-
+    name: {
+        type: String,
+        requied: true,
+    },
+    mobile: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+    },
+    email: {
+        type: String,
+        requied: true,
+    },
+    address: {
+        type: String,
+    },
 })
 
 const vendorModel = mongoose.model("Ventors", vendorSchema);
