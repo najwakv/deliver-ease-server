88eff9cdc5accede780e7cc9e6682f83b08d089d
najwa <najwa.kv20@gmail.com>
2023-09-07 20:48:02 +0530

    adminModel created

--- models/adminModel.js
+++ models/adminModel.js
@@ -4,9 +4,11 @@ const Schema = mongoose.Schema;
 const adminSchema = new Schema({
     email: {
         type: String,
+        required: true,
     },
     password: {
         type: String,
+        required: true,
     }
 });
 
