704ad3661e82b3ce76d9e6532399579ba2533de5
najwa <najwa.kv20@gmail.com>
2023-09-07 20:53:29 +0530

    driverModel created

--- models/driverModel.js
+++ models/driverModel.js
@@ -2,7 +2,25 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const driverSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true,
+    },
+    mobile: {
+        type: Number,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+    },
+    license: {
+        type: String,
+        required: true,
+    },
 });
 
 const driverModel = mongoose.model("Driver", driverSchema);
