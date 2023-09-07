96b9c2ce7061ab70b5b883560ab0d3ddfeb6122b
najwa <najwa.kv20@gmail.com>
2023-09-07 21:25:23 +0530

    orderModel created

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,9 +1,41 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({
+const Objectid = mongoose.Types.ObjectId;
 
+const orderSchema = new Schema({
+    products: [
+        {
+            product: {
+                type: Objectid,
+                ref: "Product",
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    driver: {
+        type: Objectid,
+        ref: "Driver",
+        required: true,
+    },
+    vendor: {
+        type: Objectid,
+        ref: 'Vendor',
+        required: true,
+    },
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    collectedAmount: {
+        type: Number,
+        required: true,
+    },
 });
 
-const orderModel = mongoose.model("Orders", orderSchema);
+const orderModel = mongoose.model("Order", orderSchema);
 export default orderModel;
