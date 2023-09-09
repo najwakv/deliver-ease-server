41a38348f7d477713028fa49763c286bd0466a14
najwa <najwa.kv20@gmail.com>
2023-09-09 11:40:44 +0530

    Started working on Vendor management.

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDriver, deleteDriver, doLogin, getDriver, getDrivers, toggleBlockDriver, updateDriver } from "../controllers/adminController.js";
+import { addDriver, addVendor, deleteDriver, doLogin, getDriver, getDrivers, toggleBlockDriver, updateDriver } from "../controllers/adminController.js";
 import { adminAuthMiddleware } from "../middlewares/adminAuthMiddleware.js";
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.get("/drivers", adminAuthMiddleware, getDrivers);
 router.get("/drivers/:driverId", adminAuthMiddleware, getDriver);
 router.post("/login", doLogin);
 router.post("/drivers/addDriver", adminAuthMiddleware, addDriver);
+router.post("/vendors/addVendor", adminAuthMiddleware, addVendor);
 router.patch("/drivers/:driverId", adminAuthMiddleware, updateDriver);
 router.patch("/drivers/:driverId/toggleBlockDriver", toggleBlockDriver);
 router.delete("/drivers/:driverId", adminAuthMiddleware, deleteDriver);
